d23e7758f584c6da6a9cdfec243b8c995a9fb5bf
alyssajcrawford1 <alyssajcrawford1@email.arizona.edu>
2023-12-05 16:58:54 -0600

    light/dark mode in TS

--- script/lightdarkmode.ts
+++ script/lightdarkmode.ts
@@ -1,42 +1,38 @@
-var darkModeButton = document.querySelector(".darkModeButton");
-var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-var currentTheme = localStorage.getItem("theme");
+var darkModeButton: HTMLElement = document.querySelector(".darkModeButton") as HTMLElement;
+var prefersDarkScheme: MediaQueryList = window.matchMedia("(prefers-color-scheme: dark)") as MediaQueryList;
+
+var currentTheme: string = localStorage.getItem("theme") as string;
 if (currentTheme == "dark") {
     document.body.classList.toggle("darkMode");
     darkModeButton.innerText = "light mode";
-}
-else if (currentTheme == "light") {
+} else if (currentTheme == "light") {
     document.body.classList.toggle("lightMode");
     darkModeButton.innerText = "dark mode";
-}
-else {
+} else {
     if (prefersDarkScheme) {
         darkModeButton.innerText = "light mode";
-    }
-    else {
+    } else {
         darkModeButton.innerText = "dark mode";
     }
 }
-var newTheme = "dark";
-darkModeButton.addEventListener("click", function () {
+
+var newTheme: string = "dark"
+darkModeButton.addEventListener("click", function() {
     if (prefersDarkScheme) {
         document.body.classList.toggle("lightMode");
         if (document.body.classList.contains("lightMode")) {
             newTheme = "light";
             darkModeButton.innerText = "dark mode";
-        }
-        else {
+        } else {
             newTheme = "dark";
             darkModeButton.innerText = "light mode";
         }
-    }
-    else {
+    } else {
         document.body.classList.toggle("darkMode");
         if (document.body.classList.contains("darkMode")) {
             newTheme = "dark";
             darkModeButton.innerText = "light mode";
-        }
-        else {
+        } else {
             newTheme = "light";
             darkModeButton.innerText = "dark mode";
         }
